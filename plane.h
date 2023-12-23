2034fa1e33732c01c3e9317f02bbc09cdf355ecd
CerenY1 <94191821+CerenY1@users.noreply.github.com>
2023-12-23 13:55:34 +0300

    plane.h

--- plane.h
+++ plane.h
@@ -1,120 +1,129 @@
 #ifndef PLANE_H
 #define PLANE_H
+#include "Staff.h"
 #include <iostream>
 #include <iomanip>
-#include <chrono>
+#include <ctime>
 #include <string>
 
 using namespace std;
 class Plane{
 	protected:
-		double departureTime;//hareket saati
-		double estimatedArrivalTime;
-		int travelDuration;//seyehat süresi
+		tm departureTime;//hareket saati
+		tm estimatedArrivalTime;
+		int travelDuration;//seyehat sÃ¼resi
 		int passengerCapacity;
 		double price;
-		int speed;
 		string model;
 		string departureCity;
 		string landingCity;
-		string planeSt;//uçaðýn durumu(havada mý,kalkacak mý, indi mi)
 		
-    Plane(const double depTime, int duration, const string mdl, int capacity,double price,string depCity,string landCity)
+    Plane(tm depTime,tm estimatedArrivalTime, int duration, const string mdl, int capacity,double price,string depCity,string landCity)
 	: departureTime(depTime), travelDuration(duration), model(mdl), passengerCapacity(capacity), price(price),departureCity(depCity),landingCity(landCity) {}
 	
-	double calculateArrivalTime(){
+	/*double calculateArrivalTime(){
 		int minute,hours;
 		hours = travelDuration/60;
 		minute = travelDuration%60;
 		estimatedArrivalTime = departureTime+hours+(minute*0.1);
-		return estimatedArrivalTime;	
-	}
+		return estimatedArrivalTime;
+		
+	}*/
 	
 	void displayInfo() {
-        cout << "Departure Time: " <<fixed<<setprecision(2)<< departureTime << endl;
-        cout << "Estimated Arrival Time: " <<fixed<< setprecision(2)<<calculateArrivalTime() << endl;
-        cout << "Travel Duration: " << this->travelDuration << endl;
+       // cout << "Departure Time: " <<fixed<<setprecision(2)<< departureTime << endl;
+       // cout << "Estimated Arrival Time: " <<fixed<< setprecision(2)<<calculateArrivalTime() << endl;
+       // cout << "Travel Duration: " << travelDuration/60 <<"."<< travelDuration%60<<" hours " << endl;
         cout << "Model: " << model << endl;
         cout << "Passenger Capacity: " << passengerCapacity << " passengers" << endl;
         cout << "Price: " << price << "$" << endl;
+		cout<< "-------------------------------------------" <<endl;
+        
     }	
-    virtual void calculateFlightTime(int distance){
-		this->travelDuration= distance / (this->speed);
-		cout<<"Estimated travel duration : "<<this->travelDuration<<endl;
-	}
 }; 
 
-class Pegasus1: public Plane{
-	
+class CommercialPlane : public Plane{
 	public:
-	    Pegasus1(const double depTime, int duration, const string mdl, int capacity=2,double price=27.0,const speed= 100)
-	    :Plane(depTime, duration, mdl, capacity, price) {}
 	
-	void disPlayInfo(){
+    CommercialPlane(tm deptime, tm estArrivalTime, int duration, const string mdl, int capacity, double price, string depCity, string landCity)
+    : Plane(deptime, estArrivalTime, duration, mdl, capacity, price, depCity, landCity) {}
+
+    void disPlayInfo(){
 		
 	    Plane::displayInfo();
 	    
 	}
-	void calculateFlightTime(int distance)const override{
-		this->travelDuration= distance / (this->speed);
-		cout<<"Estimated travel duration : "<<this->travelDuration<<endl;
-	}
-	
-}; 
 
-class Pegasus2: public Plane{
+};
+
+class PrivatePlane : public Plane {
+public:
+	friend class Staff;
 	
-	public:
-	    Pegasus2(const double depTime, int duration, const string mdl, int capacity=5,double price=65.0,speed= 200)
-	    :Plane(depTime, duration, mdl, capacity, price) {}
-	
-	void disPlayInfo(){
-		
-	    Plane::displayInfo();
-	    
-	}
-	void calculateFlightTime(int distance)const override{
-		this->travelDuration= distance / (this->speed);
-		cout<<"Estimated travel duration : "<<this->travelDuration<<endl;
-	}
-	
-}; 
+	string pilotName;
+
+    PrivatePlane(/*tm depTime, tm estArrivalTime,*/ int duration, const string mdl, int capacity)
+    : Plane(/*deptime, estArrivalTime,*/ duration, mdl, capacity) {}
+
+    void pilotList(){
+    	cout<<"Choose pilot name: "<<endl;
+    	
+    }
+    
+  
+
+    void displayInfo() {
+        Plane::displayInfo();
+        cout << "Pilot: " << pilotName << std::endl;
+    }
+    
+    void setPilotName(const string pilot) {
+        pilotName = pilot;
+    }
+};
+
+class CargoPlane : public Plane {
+public:
+    double cargoCapacity;
+    double currentCargoWeight;
+
+    CargoPlane(/*tm depTime, tm estArrivalTime,*/ int duration, const string mdl, int capacity, double cargoCap)
+        : Plane(/*depTime, estArrivalTime,*/ duration, mdl, capacity), cargoCapacity(cargoCap) {}
+
+    void displayInfo() {
+        Plane::displayInfo();
+        std::cout << "Cargo Capacity: " << cargoCapacity << " tons" << std::endl;
+    
+    
+    void addCargo(double weight) {
+        if (currentCargoWeight + weight <= cargoCapacity) {
+            currentCargoWeight += weight;
+            cout << "Cargo added. Current Cargo Weight: " << currentCargoWeight << " tons" << std::endl;
+        } 
+		else 
+            cout << "Cargo not added. Exceeds cargo capacity." << std::endl;
+        
+	    }
+    }
+};
+
+class MilitaryPlane : public Plane {
+public:
+    int weaponCapacity;
+    string missionType;
+
+    MilitaryPlane(/*tm depTime, tm estArrivalTime,*/ int duration, const string mdl, int capacity, int weaponCap)
+        : Plane(depTime, estArrivalTime, duration, mdl, capacity), weaponCapacity(weaponCap) {}
+        
+    void displayInfo() {
+        Plane::displayInfo();
+        std::cout << "Weapon Capacity: " << weaponCapacity << " units" << std::endl;
+    }
+    
+    void setMissionType(const string mission) {
+        missionType = mission;
+    }
+};
 
 
-class THY1: public Plane{
-	
-	public:
-	    THY1(const double depTime, int duration, const string mdl, int capacity=6,double price=30.0,speed=150)
-	    :Plane(depTime, duration, mdl, capacity, price) {}
-	
-	void disPlayInfo(){
-		
-	    Plane::displayInfo();
-	    
-	}
-	void calculateFlightTime(int distance)const override{
-		this->travelDuration= distance / (this->speed);
-		cout<<"Estimated travel duration : "<<this->travelDuration<<endl;
-	}
-	
-}; 
-
-class THY2: public Plane{
-	
-	public:
-	    THY2(const double depTime, int duration, const string mdl, int capacity=4,double price=50.0,speed=250)
-	    :Plane(depTime, duration, mdl, capacity, price) {}
-	
-	void disPlayInfo(){
-		
-	    Plane::displayInfo();
-	    
-	}
-	void calculateFlightTime(int distance)const override{
-		this->travelDuration= distance / (this->speed);
-		cout<<"Estimated travel duration : "<<this->travelDuration<<endl;		
-	}
-	
-}; 
 #endif
-
