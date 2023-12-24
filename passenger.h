12db16abf03e286985640f0a3596f9fcfe9fc899
SezinKorogluu <118663626+SezinKorogluu@users.noreply.github.com>
2023-12-24 13:58:50 +0300

    Update passenger.h

--- passenger.h
+++ passenger.h
@@ -1,5 +1,6 @@
 #include <vector>
 #include "Ticket.h"
+#include <iostream>
 using namespace std;
 
 class Passenger{
@@ -7,16 +8,21 @@ class Passenger{
 		string name;
 		string gender;
 		int age;
+		string currentLocation;
+		string arrivalLocation;
 		vector<Ticket> purchasedTickets;
 		
 	public:
-		string getName() { return name;}
-        string getGender() { return gender;}
+		 /*string getName() { return name;}
+        tring getGender() { return gender;}
         int getAge() { return age;}
         vector<Ticket> getPurchasedTickets() {return purchasedTickets;}
-        void setName(string& n) { strcpy(name, n);}
-        void setGender(string& g) {strcpy(gender, g);}
-        void getAge(int a) {age = a;}
+        void setName() {
+			cout << "Enter your name: ";
+			cin>>name;
+		} // strcpy kaldır
+        void setGender(string& g) {strcpy(gender, g);} // strcpy kaldır
+        void getAge(int a) {age = a;} kaldırdık */ 
         void addTicket(Ticket& ticket) {purchasedTickets.push_back(ticket);}
 		Passenger(string n, string g, int a) : name{n}, gender{g}, age{a} {
 		}
@@ -24,5 +30,17 @@ class Passenger{
 		{
         purchasedTickets.push_back(ticket);
         }
-
+		void enterPassengerInfo(){
+			cout << "Enter your current location: ";
+			cin >> currentLocation;
+			cout << "Enter where you want to go: ";
+			cin >> arrivalLocation;
+			cout << "Enter your name: ";
+			getline(name);
+			cout << "Enter your gender (M/F) :";
+			cin >> gender;
+			cout << "Enter your age: ";
+			cin >> age; // 18 yaşından büyük küçük olma durumuna sonra bak.	
+		}    	
+    
 };
