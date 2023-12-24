b8949c3a1c6e5fee2a5864dd6377a7e97f6ef27c
Zeynep-Ozal <118689602+Zeynep-Ozal@users.noreply.github.com>
2023-12-24 15:03:00 +0300

    Update airports.h

--- airports.h
+++ airports.h
@@ -5,11 +5,8 @@
 #include "meteorology.h"
 using namespace std;
 
-class Airport{
-	
-	private:
-			
-		Meteorology todaysWeather;	
+class Airport{	
+	private:			
 		string airportName;
 		vector <Plane> arrivingPlanes;
 		vector <Plane> leavingPlanes;
@@ -19,18 +16,37 @@ class Airport{
 		Airport(const string name):airportName(name){}	
 	public:
 		
+		void printWelcomeAirport(Meteorology& todaysWeather){
+			cout<<"-------------welcome to "<<this->airportName <<" airport--------------"<<endl<<endl<<endl;
+			todaysWeather.printWeatherInfo();
+			cout << "TIME : " << ctime(&timeNow1);
+		}
+		void setAirportName(string name){
+			airportName = name;
+		}
 		void printAirportInfo(){
 			time_t timeNow1 = time(NULL);
-			cout << "TIME : " << ctime(&timeNow1);
+			int rowNum = 1;
 			cout<<todaysWeather.printWeatherInfo()<<endl;
-			cout<<left<<setw(20) << "Flight"
+			cout<<left<<setw(20) <<"Flight Number"
+				<<setw(20)	<< "Flight"
                 <<setw(20) << "Departure City"
                 <<setw(20)<< "Landing City"
                 <<setw(20) << "Departure Time"
                 <<setw(20) << "Arrival Time" 
-				<<setw(20)<<"    "<< endl;
+				<<setw(20)<<"Status"<< endl;
         	for (const auto& plane : arrivingPlanes) {
-            cout<<setw(20)<<plane.model
+            cout<< setw(5) << rowNumber++
+				<<setw(20)<<plane.model
+                <<setw(20)<<plane.departureCity
+                <<setw(20)<<plane.landingCity
+                <<setw(20)<<plane.departureTime
+                <<setw(20)<<plane.estimatedArrivalTime
+				<<setw(20)<<plane.planeSt<<endl;
+        	}
+        	for (const auto& plane : leavingPlanes) {
+            cout<< setw(5) << rowNumber++
+				<<setw(20)<<plane.model
                 <<setw(20)<<plane.departureCity
                 <<setw(20)<<plane.landingCity
                 <<setw(20)<<plane.departureTime
@@ -41,16 +57,15 @@ class Airport{
 		vector <Plane> getArrivingPlanes(){
 			return arrivingPlanes;
 		}
-		void addPlaneToAirport(Plane p1){
+		void addArrivingPlaneToAirport(Plane p1){
 			arrivingPlanes.push_back(p1);
 		}
-		void deletePlaneFromAirport(Plane p1){
-			Plane planeToDelete;
-			auto planeToDelete = find(PlanesAtTheAirpot.begin(),PlanesAtTheAirpot.end(),p1);	
+		void addLeavingPlaneToAirport(Plane p1){
+			leavingPlanes.push_back(p1);
 		}
-		void checkFlights(vector <Plane>&arrivingPlanes, vector <Plane>&leavingPlanes){
+		void checkFlights(){
 			time_t timeNow = time(NULL);
-			for (int i=0; i<arrivingPlanes.size(); i++){
+			for (int i=0; i<this->arrivingPlanes.size(); i++){
 				if(timeNow == arrivingPlanes(i).estimatedArrivalTime || timeNow > arrivingPlanes(i).estimatedArrivalTime)
 					deletePlaneFromAirport(arrivingPlanes(i));
 			}
@@ -63,11 +78,5 @@ class Airport{
 				return istToPar;
 			else if ((city1 == "paris" && city2 == "newyork")||(city1 == "newyork" && city2 == "paris"))
 				return parToNy;
-		}
-	//while (true) {
-    //    ucanUcuslariKontrolEt(ucuslar);
-    //    cout << "Bir dakika bekleniyor..." <<endl;
-    //    time_t start_time = time(NULL);
-    //    while (time(NULL) - start_time < 60) {}  // 60 saniye bekleyin
-    //}						 	
+		}						 	
 };
