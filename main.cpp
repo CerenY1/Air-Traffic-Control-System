925c136233728863c852f764a04149463d83f314
Zeynep-Ozal <118689602+Zeynep-Ozal@users.noreply.github.com>
2023-12-24 15:02:28 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,23 +1,119 @@
-#include "plane.h"
-#include <string>
 #include <iostream>
-
+#include <string>
+#include <ctime>
+#include <iomainip>
+#include <vector>
+#include "airports.h"
+#include "plane.h"
+#include "ticket.h"
+#include "passenger.h"
+#include "meteorology.h
 using namespace std;
 
-
-int main(){
-	CommercialPlane Pegasus1("Boeing-707",175,27.0,"a","b");
-	Pegasus1.disPlayInfo();
-	CommercialPlane Pegasus2("Boeing-707",300,65.0,"a","b");
-	Pegasus2.disPlayInfo();
-	CommercialPlane THY1("Boeing-707",250,30.0,"a","b");
-	THY1.disPlayInfo();
-	CommercialPlane THY2("Boeing-707",150,50.0,"a","b");
-	THY2.disPlayInfo();
-	PrivatePlane privatePlane( 4, "Cessna Citation", 8);
-    CargoPlane cargo( 3, "Airbus A330", 200, 50.5);
-    MilitaryPlane military( 4, "F-16", 1, 6);
+int main(int argc, char** argv) {
+	int choice;
+	vector <Plane> planesIst;
+	vector <Plane> planesNy;
+	vector <Plane> planesPar;
+	Airport istanbul;
+	Airport paris;
+	Airport newyork;
+	Meteorology todaysWeather;
+	istanbul.setAirportName(istanbul);
+	newyork.setAirportName(newyork);
+	paris.setAirportName(paris);
+	cout<<"choose one:"<<endl<<"1. Staff"<<endl<<"2. Passenger"<<endl;
+	cin>>choice;
 	
+	if(choice){	
+		Staff staff;
+		Plane* plane = new Plane ;
+		plane = staff.takeFlightInfo();	
+		if (plane.departureCity == "istanbul"||plane.landingCity== "istanbul"){
+			planesIst.push_back(plane);
+			if(plane.landingCity == "istanbul")
+				istanbul.addArrivingPlaneToAirport(plane);
+			else 
+				istanbul.addLeavingPlaneToAirport(plane);
+		}
+		if (plane.departureCity =="paris"||plane.landingCity== "paris"){
+			planesPar.push_back(plane);
+			if(plane.landingCity == "paris")
+				paris.addArrivingPlaneToAirport(plane);
+			else 
+				paris.addLeavingPlaneToAirport(plane);
+		}
+		else if (plane.departureCity == "newyork"||plane.landingCity== "newyork"){
+			planesNy.push_back(plane);
+			if(plane.landingCity == "newyork")
+				newyork.addArrivingPlaneToAirport(plane);
+			else 
+				newyork.addLeavingPlaneToAirport(plane);
+		}
+	}
 	
-	return 0; 
+	else{
+		Passenger passenger;
+		passenger.enterPassengerInfo();
+		int rowNum = 0;
+		if (passenger.currentLocation == "istanbul"){
+			printWelcomeAirport(todaysWeather);
+			while (rowNum == 0) {
+    			printAirportInfo();
+    			int rowNum;
+				cout<<"enter the plane number to buy a ticket:"
+				cin>>rowNum;
+				if(rowNum >=istanbul.arrivingPlanes.size()){
+					rowNum = rowNum - istanbul.arrivingPlanes.size();
+					istanbul.leavingPlanes[rowNum];
+				}
+			 	else istanbul.arrivingPlanes[rowNum];
+    			cout << "waiting for update..." <<endl;
+    			cin>>opNum;
+    			time_t start_time = time(NULL);
+    			while (time(NULL) - start_time < 60) {}
+    		}
+		}
+		else if (passenger.currentLocation =="paris"){
+			printWelcomeAirport(todaysWeather);
+			while (rowNum == 0) {
+    			printAirportInfo();
+    			int rowNum;
+				cout<<"enter the plane number to buy a ticket:"
+				cin>>rowNum;
+				if(rowNum >=paris.arrivingPlanes.size()){
+					rowNum = rowNum - paris.arrivingPlanes.size();
+					paris.leavingPlanes[rowNum];
+				}
+			 	else paris.arrivingPlanes[rowNum];
+    			cout << "waiting for update..." <<endl;
+    			cin>>opNum;
+    			time_t start_time = time(NULL);
+    			while (time(NULL) - start_time < 60) {}
+    		}
+			
+		}
+		else if (passenger.currentLocation == "newyork"){
+			printWelcomeAirport(todaysWeather);
+			while (rowNum == 0) {
+    			printAirportInfo();
+    			int rowNum;
+				cout<<"enter the plane number to buy a ticket:"
+				cin>>rowNum;
+				if(rowNum >=newyork.arrivingPlanes.size()){
+					rowNum = rowNum - newyork.arrivingPlanes.size();
+					newyork.leavingPlanes[rowNum];
+				}
+			 	else newyork.arrivingPlanes[rowNum];
+    			cout << "waiting for update..." <<endl;
+    			cin>>opNum;
+    			time_t start_time = time(NULL);
+    			while (time(NULL) - start_time < 60) {}
+    		}
+			
+		}
+		else 
+		cout << "location not found.."<< endl;
+	}
+	return 0;
 }
