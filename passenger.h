06441d6ae70018ec7c5fb066c3bfc75ad1a445cc
Zeynep-Ozal <118689602+Zeynep-Ozal@users.noreply.github.com>
2023-12-25 22:13:18 +0300

    Update passenger.h

--- passenger.h
+++ passenger.h
@@ -1,85 +1,51 @@
+#ifndef UNTITLED2_PASSENGER_H
+#define UNTITLED2_PASSENGER_H
+
 #include <vector>
 #include "Ticket.h"
 #include <iostream>
 using namespace std;
 
 class Passenger{
-	private:
-		string name;
-		string gender;
-		int age;
-		string currentLocation;
-		string arrivalLocation;
-		vector<Ticket> purchasedTickets;
-		
-	public:
-		 /*string getName() { return name;}
-        tring getGender() { return gender;}
-        int getAge() { return age;}
-        vector<Ticket> getPurchasedTickets() {return purchasedTickets;}
-        void setName() {
-			cout << "Enter your name: ";
-			cin#ifndef PASSENGER_H
-#define PASSENGER_H
-#include <vector>
-#include "Ticket.h"
-#include <iostream>
-using namespace std;
+private:
+    string name;
+    string gender;
+    int age;
+    string currentLocation;
+    string arrivalLocation;
+    vector<Ticket> purchasedTickets;
+public:
+    Passenger(string n, string g, int a) : name{n}, gender{g}, age{a} {}
+    string getName() { return name;}
+    string getGender() { return gender;}
+    int getAge() { return age;}
+    void setName(string name1){name1 = this->name;}
+    void setGender(string gender1){gender1 = this->gender;}
+    void setAge(int age1){age1 = this->age;}
+    void disPlayInfoCommercial(){
+        cout << "Passenger's name:" << name << endl;
+    }
+    void disPlayInfoCargo(){
+        cout << "Send by:" << name << endl;
+    }
+    void disPlayInfoPrivate(){
+        cout << "Plane owner:" << name << endl;
+    }
+    void addTicket(Ticket& ticket) {purchasedTickets.push_back(ticket);}
 
-class Passenger{
-	private:
-		string name;
-		string gender;
-		int age;
-		string currentLocation;
-		string arrivalLocation;
-		vector<Ticket> purchasedTickets;
-		
-	public:
-		void enterPassengerInfo(){
-			cout << "Enter your current location: ";
-			cin >> currentLocation;
-			cout << "Enter where you want to go: ";
-			cin >> arrivalLocation;
-			cout << "Enter your name: ";
-			cin.ignore(); // Ignore any previous newline character in the buffer
-        	getline(cin, name);
-		}   
-		void disPlayInfoCommercial(){
-			cout << "Passenger's name:" << name << endl;
-		} 	
-		void disPlayInfoCargo(){
-			cout << "Send by:" << name << endl;
-		}
-		void disPlayInfoPrivate(){
-			cout << "Plane owner:" << name << endl;
-		}
-        void addTicket(const Ticket& ticket) {purchasedTickets.push_back(ticket)}
-    
+    void enterPassengerInfo(){
+        cout << "Enter your current location: ";
+        cin >> currentLocation;
+        cout << "Enter where you want to go: ";
+        cin >> arrivalLocation;
+        cout << "Enter your name: ";
+        cin.ignore();
+        getline(cin,name);
+        cout << "Enter your gender (M/F) :";
+        cin >> gender;
+        cout << "Enter your age: ";
+        cin >> age; // 18 yaşından büyük küçük olma durumuna sonra bak.
+    }
 };
 
-#endif>>name;
-		} // strcpy kaldır
-        void setGender(string& g) {strcpy(gender, g);} // strcpy kaldır
-        void getAge(int a) {age = a;} kaldırdık */ 
-        void addTicket(Ticket& ticket) {purchasedTickets.push_back(ticket);}
-		Passenger(string n, string g, int a) : name{n}, gender{g}, age{a} {
-		}
-		void addTicket(const Ticket& ticket) 
-		{
-        purchasedTickets.push_back(ticket);
-        }
-		void enterPassengerInfo(){
-			cout << "Enter your current location: ";
-			cin >> currentLocation;
-			cout << "Enter where you want to go: ";
-			cin >> arrivalLocation;
-			cout << "Enter your name: ";
-			getline(name);
-			cout << "Enter your gender (M/F) :";
-			cin >> gender;
-			cout << "Enter your age: ";
-			cin >> age; // 18 yaşından büyük küçük olma durumuna sonra bak.	
-		}    	
-    
-};
+#endif //UNTITLED2_PASSENGER_H
