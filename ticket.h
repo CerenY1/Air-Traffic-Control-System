1f176ba23a011be0c2336ddd17e274bd71476c59
Zeynep-Ozal <118689602+Zeynep-Ozal@users.noreply.github.com>
2024-01-02 14:53:09 +0300

    Update ticket.h

--- ticket.h
+++ ticket.h
@@ -10,51 +10,31 @@ class Ticket{
 private:
     string departureCity;
     string arrivalCity;
-    int seatNum;
-    double price;
     string dateAndTime;
-    int travelDuration;
-    int passengerIdentity;
-
 public:
     friend class passenger;
     Ticket(string dep = "",string arr = "", int seat = 0, double cost = 0,string dateTime = "", int duration = 0, int identity = 0)
-            : departureCity(dep), arrivalCity(arr), seatNum(seat), price(cost), dateAndTime(dateTime), travelDuration(duration), passengerIdentity(identity){}
+            : departureCity(dep), arrivalCity(arr), dateAndTime(dateTime){}
     virtual void showBill(int ticketPrice){
         cout << "Bill Information:" << endl;
-    } // abstraction ya da polimorfizm için tekrar bak.
-    bool checkPlaneSt(Plane& plane)
-    {
-        if(plane.PlaneSt == "Available")
-            return 1;
-        else
-            return 0;
     }
     virtual ~Ticket(){}
 };
-
 class CommercialTicket : public Ticket{
     friend class Plane;
     int ticketNumbers = 0;
     int price = 0;
-
 public:
     int sellTicket(CommercialPlane& commertialPlane){
         int totalPrice = 0;
-        if(commertialPlane.checkPlaneSt()){
+        if(commertialPlane.getPlaneSt()  == "Available"){
             cout << "How many tickets do you want to buy?" << endl;
             cin >> ticketNumbers;
             if(commertialPlane.getTicketNum() >= ticketNumbers) {
-                if (commertialPlane.getTicketNum() - ticketNumbers >= 0) {
-                    commertialPlane.setTicketNum(ticketNumbers);
-                    totalPrice = ticketNumbers * (commertialPlane.getTicketPrice());
-                    totalPrice = ticketNumbers * 100;
-                    cout << ticketNumbers << " ticket(s) is/are sold." << endl;
-                    cout << commertialPlane.getTicketNum() - (ticketNumbers) << endl;
-                    price = commertialPlane.getTicketPrice();
-                    ticketNumbers = 0;
-                    commertialPlane.setTicketNum(ticketNumbers);
-                }
+                commertialPlane.setTicketNum(ticketNumbers);
+                totalPrice = ticketNumbers * 250;
+                cout << ticketNumbers << " ticket(s) is/are sold." << endl;
+                price = commertialPlane.getTicketPrice();
             }
             else{
                 cout << "There is no enough ticket." << endl;
@@ -67,32 +47,28 @@ public:
     }
     virtual void showBill(int totalPrice)override{
         cout << "-------------------------" << endl;
-        cout << "Ticket price:" << 100 << "$" << endl;
+        cout << "Ticket price:" << 250 << "$" << endl;
         cout << "Total Price:" << totalPrice << "$" << endl;
         cout << "-------------------------" << endl;
-        // cout <<"Plane Information:"<< endl;
     }
-
 };
 
 class CargoTicket : public Ticket{
-    int *price;
     int cargoWeight = 0;
-
 public:
-    int sellTicket(CargoPlane& plane){
+    int sellTicket(CargoPlane& plane) {
         int totalPrice = 0;
-        if(plane.checkPlaneSt())
-        {   totalPrice= 0;
+        if (plane.getPlaneSt() == "Available") {
             cout << "How many kilograms are your cargo?" << endl;
             cin >> cargoWeight;
-            plane.addCargo(cargoWeight);
-            totalPrice = plane.getTicketPrice()*cargoWeight;
-            *price = plane.getTicketPrice();
+            if (plane.getCargoCapacity() >= cargoWeight) {
+                totalPrice = 0;
+                plane.addCargo(cargoWeight);
+                totalPrice = 100 * cargoWeight;
+            }
         }
-        else{
+        else
             cout << "Plane is not available." << endl;
-        }
         return totalPrice;
     }
     virtual void showBill(int totalPrice)override{
@@ -109,14 +85,17 @@ class PrivateTicket : public Ticket{
     string name;
     const int ticketPrice=5000;
 public:
-
-    void sellTicket(PrivatePlane& privatePlane){
-        name = privatePlane.pilotList();
+   int sellTicket(PrivatePlane& plane){
+        name = plane.pilotList();
+        cout<<"ticket price for private plane: 500 $."<<endl;
+        cout<<"1 ticket for private plane is sold..."<<endl;
+        plane.displayInfo();
+        return 1;
     }
     virtual void showBill(int totalPrice)override{
         cout << "-------------------------" << endl;
-        cout << "Pilot Name: "<< name << endl;
         cout<< "Price : " << ticketPrice << "$"<< endl;
+
     }
 };
 #endif
