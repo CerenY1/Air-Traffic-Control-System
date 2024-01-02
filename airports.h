fd04d95cb184cfa065e69e3cbf2f30fe303b3bc9
Zeynep-Ozal <118689602+Zeynep-Ozal@users.noreply.github.com>
2024-01-02 14:51:29 +0300

    Update airports.h

--- airports.h
+++ airports.h
@@ -4,38 +4,34 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include <iomanip>
 #include <sstream>
 #include "plane.h"
 #include "meteorology.h"
 using namespace std;
 
-class Airport{
+class Airport {
 private:
     string airportName;
-
 public:
-    vector <CommercialPlane> CommercialPlanes;
-    vector <CargoPlane>CargoPlanes;
-    vector <MilitaryPlane> PrivatePlanes;
+    vector<CommercialPlane> CommercialPlanes;
+    vector<CargoPlane> CargoPlanes;
+    vector<PrivatePlane> PrivatePlanes;
 
-    Airport(const string name):airportName(name){}
+    Airport(const string name) : airportName(name) {}
 
-    void printWelcomeAirport(Meteorology& todaysWeather){
-        cout<<"-------------welcome to "<< this->airportName <<" airport--------------"<<endl;
+    void printWelcomeAirport(Meteorology& todaysWeather) {
+        cout << "---WELCOME TO " << airportName << " AIRPORT---" << endl<<endl<<endl;
         todaysWeather.printWeatherInfo();
-        time_t timeNow = time(NULL);
-        cout << "TIME : " << ctime(&timeNow);
-    }
-    void setAirportName(string name){
-        airportName = name;
+        time_t currentTime = time(NULL);
+        cout << setfill(' ') << "TIME : " << ctime(&currentTime);
     }
     string formatTime(const tm &time) {
         ostringstream oss;
         oss << setfill('0') << setw(2) << time.tm_hour << ":" << setw(2) << time.tm_min;
         return oss.str();
     }
-    void printAirportInfo(){
-        time_t timeNow1 = time(NULL);
+    void printAirportInfo() {
         int rowNum = 1;
         cout << left << setw(10) << "Flight"
              << left << setw(20) << "Model"
@@ -44,10 +40,9 @@ public:
              << left << setw(15) << "Departure Time"
              << left << setw(15) << "Arrival Time"
              << left << setw(15) << "Status"
-             << left << setw(15) << "Capacity"
-             <<left<<setw(15)<<"Price"<<endl;
+             << left << setw(15) << "Price" << endl;
+        for (const auto &plane : CommercialPlanes) {
 
-        for (const auto& plane : CommercialPlanes) {
             cout << left << setw(10) << rowNum++
                  << left << setw(20) << "Commercial Plane"
                  << left << setw(15) << plane.departureCity
@@ -55,10 +50,9 @@ public:
                  << left << setw(15) << formatTime(plane.departureTime)
                  << left << setw(15) << formatTime(plane.estimatedArrivalTime)
                  << left << setw(15) << plane.PlaneSt
-                 << left << setw(15) <<"100$"<<endl;
+                 << left << setw(15) << "100$" << endl;
         }
-
-        for (const auto& plane : CargoPlanes) {
+        for (const auto &plane : CargoPlanes) {
             cout << left << setw(10) << rowNum++
                  << left << setw(20) << "Cargo Plane"
                  << left << setw(15) << plane.departureCity
@@ -66,20 +60,16 @@ public:
                  << left << setw(15) << formatTime(plane.departureTime)
                  << left << setw(15) << formatTime(plane.estimatedArrivalTime)
                  << left << setw(15) << plane.PlaneSt
-                << left << setw(15) <<"150$"<<endl;
-
+                 << left << setw(15) << "150$" << endl;
         }
-
     }
-    void addPlaneToAirport(CargoPlane& p1) {
+    void addPlaneToAirport(CargoPlane &p1){
         CargoPlanes.push_back(p1);
-
     }
-    void addPlaneToAirport(CommercialPlane& p1){
+    void addPlaneToAirport(CommercialPlane &p1) {
         CommercialPlanes.push_back(p1);
     }
-
-    void addPlaneToAirport(PrivatePlane& p1){
+    void addPlaneToAirport(PrivatePlane &p1) {
         PrivatePlanes.push_back(p1);
     }
 };
