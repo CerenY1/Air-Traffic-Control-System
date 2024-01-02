0b9ec1557346e86c1abfa87822e2a2d31797ebd7
Zeynep-Ozal <118689602+Zeynep-Ozal@users.noreply.github.com>
2024-01-02 14:52:13 +0300

    Update passenger.h

--- passenger.h
+++ passenger.h
@@ -2,10 +2,9 @@
 #define UNTITLED2_PASSENGER_H
 
 #include <vector>
-#include "ticket.h"
 #include <iostream>
+#include "ticket.h"
 using namespace std;
-
 class Passenger{
 private:
     string name;
@@ -13,7 +12,6 @@ private:
     int age;
     string currentLocation;
     string arrivalLocation;
-
 public:
     friend class Ticket;
     vector<CargoTicket> CargoTickets;
@@ -25,36 +23,32 @@ public:
     void setName(string name1) { this->name = name1; }
     void setGender(string gender1) { this->gender = gender1; }
     void setAge(int age1) { this->age = age1; }
-    void disPlayInfoCommercial(){
-        cout << "Passenger's name:" << name << endl;
-    }
-    void disPlayInfoCargo(){
-        cout << "Send by:" << name << endl;
-    }
-    void disPlayInfoPrivate(){
-        cout << "Plane owner:" << name << endl;
-    }
     void addCargoTicket(CargoTicket& cargoTicket) {
         CargoTickets.push_back(cargoTicket);
     }
-
     void addCommercialTicket(CommercialTicket& commercialTicket) {
         CommercialTickets.push_back(commercialTicket);
     }
     void enterPassengerInfo(){
-        cout << "Enter fly from: ";
+        cout <<"Fly from (istanbul,paris,newyork) :"<<endl;
         cin >> currentLocation;
-        cout << "Enter fly to: ";
+        cout <<"Fly to (istanbul,paris,newyork):"<<endl;
         cin >> arrivalLocation;
-        cout << "Enter your name: ";
+        cout <<"Enter your name :"<<endl;
         cin.ignore();
         getline(cin,name);
-        cout << "Enter your gender (M/F) :";
+        cout <<"Enter your gender (M/F) :"<<endl;
         cin >> gender;
-        cout << "Enter your age: ";
-        cin >> age; // 18 yaşından büyük küçük olma durumuna sonra bak.
+        cout <<"Enter your age :"<<endl;
+        cin >> age;
     }
-
+    void displayInfo(){
+        cout<< "Passenger Information:"<<endl;
+        cout<<"Name: "<<name<<endl;
+        cout<<"Gender: "<<gender<<endl;
+        cout <<"Age: "<<age<<endl;
+        cout <<"--------------------------"<<endl;
+    }
     string getCurrentLocation(){
         return currentLocation;
     }
